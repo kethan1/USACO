c4b6dd062f19381163dfb93f66c3a2fb8d564d08
Kethan Vegunta <kethan@vegunta.com>
2021-07-17 18:49:28 -0700

    fix up code and add learning

--- Cow Tipping/cowtip.cpp
+++ Cow Tipping/cowtip.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-void cow_tipper(int pos1_x, int pos1_y, int pos2_x, int pos2_y, vector<vector<int>> &cows) {
-    for (int index = pos1_x; index < pos2_x + 1; index++) {
-        for (int index2 = pos1_y; index2 < pos2_y + 1; index2++) {
+void cow_tipper(int pos2_x, int pos2_y, vector<vector<int>> &cows) {
+    for (int index = 0; index < pos2_x + 1; index++) {
+        for (int index2 = 0; index2 < pos2_y + 1; index2++) {
             cows[index][index2] = 1 - cows[index][index2];  // flips the cow (converts 1 to 0 and 0 to 1)
         }
     }
@@ -23,8 +23,8 @@ int main() {
 
     for (string line; getline(input, line);) {
         vector<int> current_line = {};
-        for (char ch: line) {
-            current_line.push_back(ch - '0');
+        for (char &ch: line) {
+            current_line.push_back(ch - '0');  // converts char to int and pushes it to the vector
         }
         cows.push_back(current_line);
     }
@@ -34,7 +34,7 @@ int main() {
     for (int index = stoi(first_line) - 1; index > -1; index--) {
         for (int index2 = cows[index].size() - 1; index2 > -1; index2--) {
             if (cows[index][index2] == 1) {
-                cow_tipper(0, 0, index, index2, cows);
+                cow_tipper(index, index2, cows);
                 cow_tippers_used += 1;
             }
         }
--- Cow Tipping/cowtip.py
+++ Cow Tipping/cowtip.py
@@ -1,19 +1,19 @@
 with open("cowtip.in") as cowtip_input:
     cowtip_input.readline()
-    cows = [list(map(int, list(line))) for line in cowtip_input]
+    cows = [[*map(int, list(line.strip()))] for line in cowtip_input]
 
 
-def cow_tipper(pos1_x, pos1_y, pos2_x, pos2_y, cowsList):
-    for index in range(pos1_x, pos2_x + 1):
-        for index2 in range(pos1_y, pos2_y + 1):
-            cowsList[index][index2] = 1 - cowsList[index][index2]
+def cow_tipper(pos2_x, pos2_y, cowsList):
+    for index in range(0, pos2_x + 1):
+        for index2 in range(0, pos2_y + 1):
+            cowsList[index][index2] = 1 - cowsList[index][index2]  # flips the cow (converts 1 to 0 and 0 to 1)
 
 
 cow_tippers_used = 0
 for index in range(len(cows) - 1, -1, -1):
     for index2 in range(len(cows[index]) - 1, -1, -1):
-        if cows[index][index2] == 1:
-            cow_tipper(0, 0, index, index2, cows)
+        if cows[index][index2]:
+            cow_tipper(index, index2, cows)
             cow_tippers_used += 1
 
 with open("cowtip.out", "w") as cowtip_output:
