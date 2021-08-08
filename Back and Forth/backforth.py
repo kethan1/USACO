69d3b68c7839411b424def5ece529ce4df6553bc
Kethan Vegunta <kethan@vegunta.com>
2021-08-08 11:36:37 -0700

    make slightly more effiecient

--- Back and Forth/backforth.py
+++ Back and Forth/backforth.py
@@ -1,27 +1,22 @@
-with open('backforth.in', 'r') as file1:
-    A = list(map(int, file1.readline().strip().split()))
-    B = list(map(int, file1.readline().strip().split()))
+with open('backforth.in') as input_file:
+    A = list(map(int, input_file.readline().split()))
+    B = list(map(int, input_file.readline().split()))
 
 
-ans_list = []
+ans = set()
 
-Tue = A.copy()
-for bucket1 in Tue:
-    Wed = B.copy() + [bucket1]
+for bucket1 in A:
+    Wed = B + [bucket1]
     for bucket2 in Wed:
-        if bucket1 == bucket2:
-            Thurs = Tue.copy()
-        else:
-            Thurs = Tue.copy() + [bucket2]
+        Thurs = A.copy()
+        if bucket1 != bucket2:
+            Thurs.append(bucket2)
             Thurs.remove(bucket1)
         for bucket3 in Thurs:
-            Fri = Wed.copy() + [bucket3]
+            Fri = Wed + [bucket3]
             Fri.remove(bucket2)
             for bucket4 in Fri:
-                x = bucket1-bucket2+bucket3-bucket4
-                if x not in ans_list:
-                    ans_list.append(x)
+                ans.add(bucket1 - bucket2 + bucket3 - bucket4)
 
-
-with open('backforth.out', 'w') as file2:
-    print(len(ans_list), file=file2)
+with open('backforth.out', 'w') as output_file:
+    output_file.write(str(len(ans)))
