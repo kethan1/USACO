f6bb97effb248a76d97ae452649d44d0e6a280d2
Kethan Vegunta <kethan@vegunta.com>
2021-08-08 14:15:48 -0700

    make water.py code better

--- Water Bowls/water.py
+++ Water Bowls/water.py
@@ -1,5 +1,5 @@
-with open("water.in", "r") as file1:
-    inputList = [line.strip() for line in file1]
-with open("water.out", "w") as file2:
-    for each in [round((2/3*3.14*(int(each, 2)**3))/1000) for each in inputList]:
-        print(each, file=file2)
+with open("water.in", "r") as input_file:
+    input_list = [line.strip() for line in input_file]
+with open("water.out", "w") as output_file:
+    for binary_diameter in input_list:
+        print(round((2/3 * 3.14 * (int(binary_diameter, 2) ** 3)) / 1000), file=output_file)
