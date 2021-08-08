0de41dcaae493432c549de61488ef47ba646756c
Kethan Vegunta <kethan@vegunta.com>
2021-08-08 10:55:24 -0700

    fix angry cows code

--- Angry Cows/angry.cpp
+++ Angry Cows/angry.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
 #include <set>
 
 using namespace std;
 
 set<int> blast(set<int> hay_bales, int bale_shot, int blast_radius = 1) {
     set<int> exploded;
-    for (auto hay_bale = hay_bales.begin(); hay_bale != hay_bales.end(); hay_bale++) {
-        if (0 < abs(*hay_bale - bale_shot) && abs(*hay_bale - bale_shot) <= blast_radius) {
-            exploded.insert(*hay_bale);
+    for (int hay_bale: hay_bales) {
+        if (0 < abs(hay_bale - bale_shot) && abs(hay_bale - bale_shot) <= blast_radius) {
+            exploded.insert(hay_bale);
         }
     }
-    for (auto hay_bale_exploded = exploded.begin(); hay_bale_exploded != exploded.end(); hay_bale_exploded++) {
+    for (int hay_bale_exploded: exploded) {
         set<int> bale_chain_reaction;
-        for (auto hay_bale2 = hay_bales.begin(); hay_bale2 != hay_bales.end(); hay_bale2++) {
-            if (!(exploded.count(*hay_bale2)) || *hay_bale2 == bale_shot) {
-                bale_chain_reaction.insert(*hay_bale2);
+        for (int hay_bale2: hay_bales) {
+            if (exploded.count(hay_bale2) == 0 && hay_bale2 != bale_shot) {
+                bale_chain_reaction.insert(hay_bale2);
             }
         }
-        set<int> reaction = blast(bale_chain_reaction, *hay_bale_exploded, blast_radius + 1);
+        set<int> reaction = blast(bale_chain_reaction, hay_bale_exploded, blast_radius + 1);
         exploded.insert(reaction.begin(), reaction.end());
     }
 
@@ -27,7 +28,6 @@ set<int> blast(set<int> hay_bales, int bale_shot, int blast_radius = 1) {
 
 int main() {
     fstream input("angry.in", ios::in);
-    int N;
     set<int> g1;
 
     string line;
@@ -35,26 +35,19 @@ int main() {
 
     for (string line; getline(input, line);) {
         g1.insert(stoi(line));
-        cout << line << " ";
     }
-    cout << endl;
     input.close();
 
-    for (auto const &bale : g1) {
-        cout << bale << " ";
-    }
-    cout << endl;
+    int max_size = 0;
 
-    set<int> output_of_cow_shot;
-
-    for (auto it = g1.begin(); it != g1.end(); it++) {
-        set<int> chain_reaction_output = blast(g1, *it);
-        chain_reaction_output.insert(*it);
-        output_of_cow_shot.insert(chain_reaction_output.size());
+    for (int cow: g1) {
+        set<int> chain_reaction_output = blast(g1, cow);
+        chain_reaction_output.insert(cow);
+        max_size = max(max_size, int(chain_reaction_output.size()));
     }
 
     fstream MyFile("angry.out", ios::out);
-    MyFile << *output_of_cow_shot.rbegin() << endl;
+    MyFile << max_size << endl;
     MyFile.close();
 
     return 0;
