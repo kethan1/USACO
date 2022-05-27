198bff8a10623a3b74687f0a666eca3eb87dae04
Kethan Vegunta <kethan@vegunta.com>
2022-05-26 19:23:14 -0700

    make Cow Gymnastics code more efficient

--- Cow Gymnastics/gymnastics.py
+++ Cow Gymnastics/gymnastics.py
@@ -4,11 +4,7 @@ with open("gymnastics.in") as input_file:
     K, N = map(int, input_file.readline().split())
     weeks = [list(map(int, line.split())) for line in input_file]
 
-pairs = 0
-for combination in itertools.permutations(weeks[0], 2):
-    consistent = all(week.index(combination[0]) >= week.index(combination[1]) for week in weeks)
-
-    pairs += consistent
+pairs = sum(all(week.index(combination[0]) >= week.index(combination[1]) for week in weeks) for combination in itertools.permutations(weeks[0], 2))
 
 with open("gymnastics.out", "w") as output_file:
     print(pairs, file=output_file)
