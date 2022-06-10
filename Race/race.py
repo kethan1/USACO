e722f5b926c8fee73a53f205a59373cd7e2a37fd
Kethan Vegunta <kethan@vegunta.com>
2022-06-10 12:56:31 -0700

    improvement(fix solution): fix solution for Race

--- Race/race.py
+++ Race/race.py
@@ -1,21 +1,23 @@
-data = []
+with open("race.in") as input_file:
+    K, N = map(int, input_file.readline().split())
+    Xs = list(map(int, input_file.readlines()))
 
-with open('race.in') as input_file:
-    for line in input_file.readlines():
-        data.append(list(map(int, line.strip().split())))
 
-output = ''
+def summation(start, stop=0):
+    return sum(range(stop, start+1))
 
-for each in data:
-    x = each[0:3]
-    won_person = x.index(min(x))
-    if won_person == 0:
-        won_person_letter = 'A'
-    elif won_person == 1:
-        won_person_letter = 'B'
-    elif won_person == 2:
-        won_person_letter = 'C'
-    output += won_person_letter+' '+str(round(each[3]/each[won_person]))+'\n'
 
-with open('race.out', 'w') as file2:
-    print(output, file=file2)
+outputs = []
+for X in Xs:
+    time = speed = position = 0
+    while position < K:
+        if K - position - summation(speed + 1, X) >= 0:
+            speed += 1
+        elif speed > 0 and K - position - summation(speed, X) < 0:
+            speed -= 1
+        position += speed
+        time += 1
+    outputs.append(time)
+
+with open("race.out", "w") as input_file:
+    print(*outputs, sep="\n", file=input_file)
