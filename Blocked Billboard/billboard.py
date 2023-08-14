2550a37ba86c0c46480a5271bea42cf391961d2e
Kethan Vegunta <kethan@vegunta.com>
2023-08-13 17:05:37 -0700

    fix blocked billboard code

--- Blocked Billboard/billboard.py
+++ Blocked Billboard/billboard.py
@@ -1,32 +1,56 @@
 with open("billboard.in") as input_file:
-    b11x, b12y, b12x, b11y = map(int, input_file.readline().split())
-    b21x, b22y, b22x, b21y = map(int, input_file.readline().split())
-    t1x, t1y, t2x, t2y = map(int, input_file.readline().split())
+    b1x1, b1y1, b1x2, b1y2 = map(int, input_file.readline().split())
+    b2x1, b2y1, b2x2, b2y2 = map(int, input_file.readline().split())
+    tx1, ty1, tx2, ty2 = map(int, input_file.readline().split())
 
-coords = [[[x, y] for x in range(b11x, b12x+1) for y in range(b12y, b11y+1)], [[x, y] for x in range(b21x, b22x+1) for y in range(b22y, b21y+1)],[[x,y] for x in range(t1x, t2x+1) for y in range(t1y, t2y+1)]]
-area1, area2 = (b12x-b11x)*(b11y-b12y), (b22x-b21x)*(b21y-b22y)
-maxx1, maxy1, minx1, miny1, maxx2, maxy2, minx2, miny2 = -2000, -2000, 2000, 2000, -2000, -2000, 2000, 2000
-common1, common2 = [each for each in coords[2] if each in coords[0]], [each for each in coords[2] if each in coords[1]]
 
-for each in common1:
-    if each[0] > maxx1:
-        maxx1 = each[0]
-    elif each[0] < minx1:
-        minx1 = each[0]
-    if each[1] > maxy1:
-        maxy1 = each[1]
-    elif each[1] < miny1:
-        miny1 = each[1]
+class Point:
+    def __init__(self, xcoord, ycoord):
+        self.x = xcoord
+        self.y = ycoord
 
-for each in common2:
-    if each[0] > maxx2:
-        maxx2 = each[0]
-    elif each[0] < minx2:
-        minx2 = each[0]
-    if each[1] > maxy2:
-        maxy2 = each[1]
-    elif each[1] < miny2:
-        miny2 = each[1]
+
+class Rectangle:
+    def __init__(self, bottom_left, top_right):
+        self.bottom_left = bottom_left
+        self.top_right = top_right
+
+    def intersects(self, other):
+        return not (
+            self.top_right.x < other.bottom_left.x
+            or self.bottom_left.x > other.top_right.x
+            or self.top_right.y < other.bottom_left.y
+            or self.bottom_left.y > other.top_right.y
+        )
+
+    def area(self):
+        return (self.top_right.x - self.bottom_left.x) * (
+            self.top_right.y - self.bottom_left.y
+        )
+
+    def overlap(self, other):
+        if not self.intersects(other):
+            return 0
+
+        x_overlap = max(
+            0,
+            min(self.top_right.x, other.top_right.x)
+            - max(self.bottom_left.x, other.bottom_left.x),
+        )
+        y_overlap = max(
+            0,
+            min(self.top_right.y, other.top_right.y)
+            - max(self.bottom_left.y, other.bottom_left.y),
+        )
+
+        return x_overlap * y_overlap
+
+
+b1 = Rectangle(Point(b1x1, b1y1), Point(b1x2, b1y2))
+b2 = Rectangle(Point(b2x1, b2y1), Point(b2x2, b2y2))
+t = Rectangle(Point(tx1, ty1), Point(tx2, ty2))
+
+area_visible = b1.area() - b1.overlap(t) + b2.area() - b2.overlap(t)
 
 with open("billboard.out", "w") as output_file:
-    print((area1-((maxx2-minx2)*(maxy2-miny2)))+(area2-((maxx1-minx1)*(maxy1-miny1))), file=output_file)
+    print(area_visible, file=output_file)
